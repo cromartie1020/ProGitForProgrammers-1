4aa34c475568296ac4762e968566db89cc011cf9
cromartie1020 <hrc245@mail.com>
2024-12-19 00:19:02 -0500

    Changes

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -11,7 +11,7 @@ namespace ProGitForProgrammers
             Console.WriteLine("I just added this in the command line repo");
             Console.WriteLine("This line added to demonstrate messages.");
             Console.WriteLine("This line added to show the staging area");
-
+            Console.WriteLine("This line added to show the staging area");    
         }
     }
 }
