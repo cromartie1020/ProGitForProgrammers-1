dd0f151e6b5c150ef51d38694290700dccdec990
cromartie1020 <hrc245@mail.com>
2024-12-30 23:25:05 -0500

    Add DateTime class

--- program.cs
+++ program.cs
@@ -8,4 +8,10 @@ namespace ProGitForProgrammers
     {
         return left + ProGitForProgrammers right
     } 
+    public class Book
+    {
+        public string Title (git; set;)
+        public DateTime PublicationDate{get; set; }
+    }
+
 }
