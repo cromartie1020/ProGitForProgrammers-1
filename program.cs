00348bd8a8cfe66ac80275231efa2957daf4f966
cromartie1020 <hrc245@mail.com>
2024-12-30 22:42:06 -0500

    Add the add method

--- program.cs
+++ program.cs
@@ -2,6 +2,10 @@ namespace ProGitForProgrammers
 {
     public class Calculator
     {
-        
+
     }
+    public int Add(int left, int right)
+    {
+        return left + ProGitForProgrammers right
+    } 
 }
